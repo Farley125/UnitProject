8bbd23ac8b1f3713510a0b748792708f8abb0423
BT_4N2_30 <fasfaf>
2022-11-10 12:33:02 -0500

    completed methods and added way for user to interact with the program

--- src/GameMethods.java
+++ src/GameMethods.java
@@ -1,6 +1,8 @@
 public class GameMethods {
     private String userName;
+    private String artifact;
     private double critRate;
+    private double evadeChance;
     private int health;
     private int speed;
     private int strength;
@@ -8,10 +10,12 @@ public class GameMethods {
     private int potion;
     private int attackCounter;
     private int playerNumber;
+    private int allocatableStat;
 private boolean havePotion;
-    public GameMethods (String name, double critRate, int health, int speed, int strength, int fortitude, int potion, int playerNumber) {
+    public GameMethods (String name, String artifact, double critRate, int health, int speed, int strength, int fortitude, int potion, int playerNumber) {
         //A constructor with name, stats, and the potion that they may or may not have.
         this.userName = name;
+        this.artifact = artifact;
         this.critRate = critRate;
         this.health = health;
         this.speed = speed;
@@ -20,10 +24,12 @@ private boolean havePotion;
         this.potion = potion;
         this.playerNumber = playerNumber;
         attackCounter = 0;
+        allocatableStat = 5;
+        evadeChance = .05;
         havePotion = true;
     }
 
-    public GameMethods (String name, int critRate, int health, int speed, int strength, int fortitude) {
+    public GameMethods (String name, double critRate, int health, int speed, int strength, int fortitude, String artifact, int playerNumber) {
        // Overloaded constructor without the potion (incase they want it disabled)
         userName = name;
         this.critRate = critRate;
@@ -31,8 +37,14 @@ private boolean havePotion;
         this.speed = speed;
         this.strength = strength;
         this.fortitude = fortitude;
+        this.artifact = artifact;
         attackCounter = 0;
+        evadeChance = .05;
+        allocatableStat = 5;
         havePotion = false;
+    }
+
+    public GameMethods (){
     }
 
     public String exchange(String user1, String user2) {
@@ -52,6 +64,7 @@ private boolean havePotion;
         {
 
         }
+        return "my nuts";
     }
 
     public void setHealth(int newHealth) {
@@ -77,6 +90,42 @@ private boolean havePotion;
 
     public boolean gameOver() {
         //will check health of users.
+return true;
+    }
 
+    public void statAdjust() {
+        if (artifact.equals("a")) {
+            strength++;
+        } else if (artifact.equals("d")) {
+            critRate += .10;
+        } else if (artifact.equals("w")) {
+            evadeChance += .10;
+            speed ++;
+        } else if (artifact.equals("v")) {
+            fortitude++;
+            health += 20;
+        }
+    }
+
+    public void pointAlloc(int strength, int speed, int fortitude)
+    {
+        this.strength += strength;
+        this.speed += speed;
+        evadeChance += (.5 * speed);
+        this.fortitude += fortitude;
+        health += (20 * (this.fortitude-1));
+    }
+
+    public void infoPrint()
+    {
+        System.out.println ("user: " + userName);
+        System.out.println ("critRate: " + critRate);
+        System.out.println ("health: " + health);
+        System.out.println ("speed: " + speed);
+        System.out.println ("strength: " + strength);
+        System.out.println ("fortitude: " + fortitude);
+        System.out.println ("potions: " + potion);
+        System.out.println ("playerNumber: " + playerNumber);
+        System.out.println ("");
     }
 }
--- src/GameRunner.java
+++ src/GameRunner.java
@@ -1,2 +1,73 @@
+import java.util.Scanner;
 public class GameRunner {
+    public static void main(String[] args) {
+        int potions = 0;
+        int availStatPts1 = 5;
+        int availStatPts2 = 5;
+        GameMethods player1 = new GameMethods();
+        GameMethods player2 = new GameMethods();
+        Scanner s = new Scanner(System.in);
+        System.out.print("Player 1, what would you like to be called? ");
+        String userName = s.nextLine();
+        System.out.print("Choose an artifact. (a for (artifact name) (+1 strength), d for (artifact name) (+15% crit chance), w for (artifact name) (+1 speed), v for (artifact name) (+1 fortitude): ");
+        String artifact  = s.nextLine();
+        System.out.print("Will you be playing with potions? (y or n): ");
+        String hasPotions = s.nextLine();
+        if (hasPotions.equals("y"))
+        {
+            player1 = new GameMethods(userName, artifact, .10, 200, 1, 1, 1, 1, 1);
+        }
+        else
+        {
+            player1 = new GameMethods(userName, .10, 200, 1, 1, 1, artifact, 1);
+        }
+        player1.statAdjust();
+        System.out.print("Player 1, how many points would you like to put into strength? (" + availStatPts1 + " points left): ");
+        String strAllocate = s.nextLine();
+        availStatPts1 -= Integer.parseInt(strAllocate);
+        System.out.print("Player 1, how many points would you like to put into speed? (" + availStatPts1 + " points left): ");
+        String spdAllocate = s.nextLine();
+        availStatPts1 -= Integer.parseInt(spdAllocate);
+        System.out.print("Player 1, how many points would you like to put into fortitude? (" + availStatPts1 + " points left): ");
+        String fortAllocate = s.nextLine();
+        availStatPts1 -= Integer.parseInt(fortAllocate);
+        player1.pointAlloc(Integer.parseInt(strAllocate), Integer.parseInt(spdAllocate), Integer.parseInt(fortAllocate));
+        System.out.println("");
+        System.out.print("Player 2, what would you like to be called? ");
+        userName = s.nextLine();
+        System.out.print("Choose an artifact. (a for (artifact name) (+1 strength), d for (artifact name) (+10% crit chance), w for (artifact name) (+1 speed), v for (artifact name) (+1 fortitude): ");
+        artifact  = s.nextLine();
+        if (hasPotions.equals("y"))
+        {
+            player2 = new GameMethods(userName, artifact, .10, 200, 1, 1, 1, 1, 2);
+        }
+        else
+        {
+            player2 = new GameMethods(userName, .10, 200, 1, 1, 1, artifact, 2);
+        }
+        player2.statAdjust();
+        System.out.print("Player 2, how many points would you like to put into strength? (" + availStatPts2 + " points left): ");
+        strAllocate = s.nextLine();
+        availStatPts2 -= Integer.parseInt(strAllocate);
+        System.out.print("Player 2, how many points would you like to put into speed? (" + availStatPts2 + " points left): ");
+        spdAllocate = s.nextLine();
+        availStatPts2 -= Integer.parseInt(spdAllocate);
+        System.out.print("Player 2, how many points would you like to put into fortitude? (" + availStatPts2 + " points left): ");
+        fortAllocate = s.nextLine();
+        availStatPts2 -= Integer.parseInt(fortAllocate);
+        if (availStatPts1 < 0 || availStatPts2 < 0)
+        {
+            System.out.print("Dirty cheater.");
+            System.exit(0);
+        }
+        player2.pointAlloc(Integer.parseInt(strAllocate), Integer.parseInt(spdAllocate), Integer.parseInt(fortAllocate));
+
+
+
+
+
+
+        player1.infoPrint();
+        player2.infoPrint();
+    }
 }
