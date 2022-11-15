af36b244af4fa935566d0a26ac25fbfb7e65fbd6
BT_4N2_30 <asfajfwiajf>
2022-11-15 12:32:54 -0500

    Added output for exchanges between user inputs. (print statements and variable value changes)

--- src/GameMethods.java
+++ src/GameMethods.java
@@ -45,41 +45,104 @@ public class GameMethods {
     public GameMethods (){
     }
 
-    public int exchange(String attacker, int baseDamage, String victim) {
+    public int exchange(String attacker, String victim) {
         //Method that'll compare the two given moves and determine the outcome of them.
+        int baseDamage = (strength * 5) + 10;
+        if (attacker.equals(""))
+        {
+            return 0;
+        }
+        if (attacker.equals("a")) {
+            attackCounter++;
+        } else {
+            if (attackCounter > 0) {
+                attackCounter--;
+            }
+        }
         if (attacker.equals("c")) {
             if (potion > 0) {
                 consume();
-                System.out.println("You reach into your bag and pull out a potion! + 50 Health! \n [You have " + potion + " potion(s)]");
             }
             else {
-                System.out.println("You reach into your bag and pull out nothing! [You have 0 potions]");
                 return 0;
             }
         }
         if (attacker.equals("p") && victim.equals("a") == false) {
             isStun = true;
-                System.out.println("You ready yourself for an attack that never came, your rhythm was broken! \n You've been stunned.");
-                return 0;
+            return 0;
+        } else if (attacker.equals("p") && victim.equals("a")) {
+            return 0;
         }
-        if (attacker.equals("a") && victim.equals("d")){
-            return (int) (Math.ceil((double)baseDamage/5));
+        if (attacker.equals("a") && victim.equals("p")) {
+            isStun = true;
+            return 0;
         }
-        return 0;
+        if (attacker.equals("a") && victim.equals("d")) {
+            double crit = Math.random();
+            if (crit < critRate) {
+                return (baseDamage / 3) * 2;
+            }
+            return baseDamage / 3;
+        } else if (attacker.equals("d")) {
+            return 0;
+        }
+        return baseDamage;
+    }
+
+    public void outcomeMessage(String attacker, String victim) {
+        int baseDamage = (strength*5) + 25;
+        if (attacker.equals("c")) {
+            if (potion > 0) {
+                System.out.println("You reach into your bag and pull out a potion! + 50 Health! \n[You have " + potion + " potion(s)]\n");
+            }
+            else {
+                System.out.println("You reach into your bag and pull out nothing! [You have 0 potions]\n");
+            }
+        }
+        else if (attacker.equals("p") && victim.equals("a") == false) {
+            System.out.println("You ready yourself for an attack that never came, your posture was disrupted! \nYou've been stunned.\n");
+        } else if (attacker.equals("p") && victim.equals("a")) {
+            System.out.println("You parried their blow!");
+        }
+        else if (attacker.equals("a") && victim.equals("d")){
+            double crit = Math.random();
+            if (crit < critRate)
+            {
+               System.out.println("You strike a defending opponent, but it was a critical! (You deal " + (baseDamage/3)*2 + " damage)\n");
+            }
+            else System.out.println("You strike a defending opponent. (You deal " + (baseDamage/3) + " damage)\n");
+        }
+        else if (attacker.equals("a") && victim.equals("p")) {
+            System.out.println("Your blow was parried, you were thrown off balance. \nYou've been stunned.\n");
+        }
+        else if (attacker.equals("d")) {
+            System.out.println("You ready your shield.\n");
+        } else if (attacker.equals(""))
+        {
+            System.out.println("You're unable to move.\n");
+        }
+        else System.out.println("You strike your opponent. (You deal " + (baseDamage) + " damage)\n");
     }
 
     public void setHealth(int newHealth) {
         //Method to set health of user.
         health = newHealth;
     }
-    public int getDamage(){
-        return strength * 5;
+    public int getHealth(){
+        return health;
     }
 
     public void consume() {
         //method to consume potion and affect stat associated
         potion--;
         setHealth(health + 50);
+    }
+
+    public boolean isStun() {
+        return isStun;
+    }
+    public void unStun() {
+        isStun = false;
     }
 
     public boolean gameOver() {
@@ -93,15 +156,14 @@ public class GameMethods {
 
     public void statAdjust() {
         if (artifact.equals("a")) {
-            strength += 2;
+            strength += 3;
         } else if (artifact.equals("d")) {
             critRate += .10;
         } else if (artifact.equals("w")) {
-            evadeChance += .10;
-            speed ++;
+            evadeChance += .15;
+            speed += 3;
         } else if (artifact.equals("v")) {
-            fortitude++;
-            health += 20;
+            fortitude += 4;
         }
     }
 
@@ -111,10 +173,10 @@ public class GameMethods {
         this.speed += speed;
         evadeChance += (.5 * speed);
         this.fortitude += fortitude;
-        health += (40 * this.fortitude);
+        health += (10 * this.fortitude);
     }
 
-    public void infoPrint()
+    public void infoPrint2()
     {
         System.out.println ("user: " + userName);
         System.out.println ("critRate: " + critRate);
@@ -123,6 +185,15 @@ public class GameMethods {
         System.out.println ("strength: " + strength);
         System.out.println ("fortitude: " + fortitude);
         System.out.println ("potions: " + potion);
+        System.out.println ("");
+    }
+
+    public void infoPrint()
+    {
+        System.out.println ("user: " + userName);
+        System.out.println ("health: " + health);
+        System.out.println ("potions: " + potion);
+        System.out.println ("attack counters: " + attackCounter);
         System.out.println ("");
     }
 }
--- src/GameRunner.java
+++ src/GameRunner.java
@@ -9,7 +9,7 @@ public class GameRunner {
         Scanner s = new Scanner(System.in);
         System.out.print("Player 1, what would you like to be called? ");
         String userName = s.nextLine();
-        System.out.print("Choose an artifact. (a for (artifact name) (+1 strength), d for (artifact name) (+15% crit chance), w for (artifact name) (+1 speed), v for (artifact name) (+1 fortitude): ");
+        System.out.print("Choose an artifact. (a for (artifact name) (+3 strength), d for (artifact name) (+10% crit chance), w for (artifact name) (+3 speed), v for (artifact name) (+4 fortitude): ");
         String artifact = s.nextLine();
         System.out.print("Will you be playing with potions? (y or n): ");
         String hasPotions = s.nextLine();
@@ -35,18 +35,22 @@ public class GameRunner {
         }
         player1.pointAlloc(Integer.parseInt(strAllocate), Integer.parseInt(spdAllocate), Integer.parseInt(fortAllocate));
 
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 40; i++) {
             System.out.println("");
+            if (i == 20)
+            {
+                System.out.print("It isn't too late to turn back.");
+            }
         }
 
         System.out.print("Player 2, what would you like to be called? ");
         userName = s.nextLine();
-        System.out.print("Choose an artifact. (a for (artifact name) (+1 strength), d for (artifact name) (+10% crit chance), w for (artifact name) (+1 speed), v for (artifact name) (+1 fortitude): ");
+        System.out.print("Choose an artifact. (a for (artifact name) (+3 strength), d for (artifact name) (+10% crit chance), w for (artifact name) (+3 speed), v for (artifact name) (+4 fortitude): ");
         artifact = s.nextLine();
         if (hasPotions.equals("y")) {
-            player2 = new GameMethods(userName, artifact, .10, 200, 1, 1, 1, 1);
+            player2 = new GameMethods(userName, artifact, .10, 200, 1, 2, 1, 1);
         } else {
-            player2 = new GameMethods(userName, .10, 200, 1, 1, 1, artifact);
+            player2 = new GameMethods(userName, .10, 200, 1, 2, 1, artifact);
         }
         player2.statAdjust();
         System.out.print("Player 2, how many points would you like to put into strength? (" + availStatPts2 + " points left): ");
@@ -65,37 +69,79 @@ public class GameRunner {
         }
         player2.pointAlloc(Integer.parseInt(strAllocate), Integer.parseInt(spdAllocate), Integer.parseInt(fortAllocate));
 
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 40; i++) {
             System.out.println("");
+            if (i == 20)
+            {
+                System.out.print("It isn't too late to turn back.");
+            }
         }
 
-        int baseDamage1 = player1.getDamage();
-        int baseDamage2 = player2.getDamage();
-        String input1;
-        String input2;
+        String input1 = "";
+        String input2 = "";
         while (player1.gameOver() == false && player2.gameOver() == false) {
+            player1.infoPrint();
+            player2.infoPrint();
 
             boolean successfulInput = false;
-            while (successfulInput != true) {
-                System.out.println("Player 1, make your move. (a to attack, d to defend, p to parry, and c to consume your health potion");
-                input1 = s.nextLine();
-                if (input1.equals("a") || input1.equals("d") || input1.equals("p") || input1.equals("c")) {
-                    successfulInput = true;
-                } else
-                    System.out.println("There was a misinput!!! Type a to attack, d to defend, p to parry, and c to consume your health potion");
+            if (player1.isStun() == false) {
+                while (successfulInput != true) {
+                    System.out.println("Player 1, make your move. (a to attack, d to defend, p to parry, and c to consume your health potion");
+                    input1 = s.nextLine();
+                    System.out.println(input1);
+                    if (input1.equals("a") || input1.equals("d") || input1.equals("p") || input1.equals("c")) {
+                        successfulInput = true;
+                        for (int i = 0; i < 40; i++) {
+                            System.out.println("");
+                            if (i == 20)
+                            {
+                                System.out.print("It isn't too late to turn back.");
+                            }
+                        }
+                    } else
+                        System.out.println("There was a misinput!!! Type a to attack, d to defend, p to parry, and c to consume your health potion");
+
+                }
+            } else {
+                System.out.println("Player 1 is stunned, they are unable to move.");
+                input1 = "";
+                player1.unStun();
             }
             successfulInput = false;
-            while (successfulInput != true) {
-                System.out.println("Player 2, make your move. (a to attack, d to defend, p to parry, and c to consume your health potion");
-                input2 = s.nextLine();
-                if (input2.equals("a") || input2.equals("d") || input2.equals("p") || input2.equals("c")) {
-                    successfulInput = true;
-                } else
-                    System.out.println("There was a misinput!!! Type a to attack, d to defend, p to parry, and c to consume your health potion");
+
+
+
+            if (player2.isStun() == false) {
+                while (successfulInput != true) {
+                    System.out.println("Player 2, make your move. (a to attack, d to defend, p to parry, and c to consume your health potion");
+                    input2 = s.nextLine();
+                    System.out.println(input2);
+                    if (input2.equals("a") || input2.equals("d") || input2.equals("p") || input2.equals("c")) {
+                        successfulInput = true;
+                        for (int i = 0; i < 40; i++) {
+                            System.out.println("");
+                            if (i == 20)
+                            {
+                                System.out.print("It isn't too late to turn back.");
+                            }
+                        }
+                    } else
+                        System.out.println("There was a misinput!!! Type a to attack, d to defend, p to parry, and c to consume your health potion");
+                }
+            } else {
+                System.out.println("Player 2 is stunned, they are unable to move.");
+                player2.unStun();
+                input2 = "";
             }
-            successfulInput = false;
+
+
+            player1.setHealth(player1.getHealth() - player2.exchange(input2, input1));
+            player1.outcomeMessage(input1, input2);
+            player2.setHealth(player2.getHealth() - player1.exchange(input1, input2));
+            player2.outcomeMessage(input2, input1);
+
+            player1.infoPrint();
+            player2.infoPrint();
         }
-        player1.infoPrint();
-        player2.infoPrint();
     }
 }
